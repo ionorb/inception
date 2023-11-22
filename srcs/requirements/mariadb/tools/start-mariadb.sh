a81ae90968d7bd64e1fa8614d603956c4ed73d23
Yoel Ridgway-Lopez <71927888+ionorb@users.noreply.github.com>
2023-11-22 14:36:10 +0000

    everything working

--- srcs/requirements/mariadb/tools/start-mariadb.sh
+++ srcs/requirements/mariadb/tools/start-mariadb.sh
@@ -4,3 +4,6 @@ echo "mysqld_safe starting..."
 
 # Start MariaDB
 exec mysqld_safe
+# Import all .cnf files from configuration directory
+# !includedir /etc/mysql/conf.d/
+# !includedir /etc/mysql/mariadb.conf.d/
--- srcs/requirements/wordpress/tools/start-wordpress.sh
+++ srcs/requirements/wordpress/tools/start-wordpress.sh
@@ -8,10 +8,26 @@ wait_for_db() {
     done
 }
 
+# Function to check if wordpress is installed
+check_for_wordpress() {
+	if ! $(wp core is-installed --path="$WP_PATH"); then
+		# Install WordPress
+		wp --allow-root core install --url="example.com" --title="Example" --admin_user="uniqueuser" --admin_password="password" --admin_email="admin@example.com" --path="$WP_PATH"
+		# Create a second user
+		wp --allow-root user create user2 user2@example.com --role=author --user_pass=password --path="$WP_PATH"
+	fi
+}
+
 # Wait for MariaDB to be ready
 wait_for_db
 
-echo "Starting FPM"
+# Path to WordPress
+WP_PATH="/var/www/html"
+
+# Check if WordPress is installed
+check_for_wordpress
+
 
 # Start php-fpm in the foreground
+echo "Starting FPM"
 exec php-fpm7.4 -F
