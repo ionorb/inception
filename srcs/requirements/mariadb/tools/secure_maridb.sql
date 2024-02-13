08f655ed3ca4c3e3bcc33875c8dbf304f11835dd
Yoel RIDGWAY-LOPEZ <yoelridgway@gmail.com>
2024-02-13 18:44:32 +0100

    changed the scripts to start on mariadb with .sq on init

--- srcs/requirements/mariadb/tools/secure_maridb.sql
+++ srcs/requirements/mariadb/tools/secure_maridb.sql
@@ -9,3 +9,22 @@ DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 -- Reload privilege tables
 FLUSH PRIVILEGES;
+
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `DB_NAME`;
+
+-- Create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'MYSQL_USER'@'%' IDENTIFIED BY 'MYSQL_PASSWORD';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON `DB_NAME`.* TO 'MYSQL_USER'@'%';
+
+-- Set root password (if necessary, adjust for MariaDB version)
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('YOUR_SECURE_PASSWORD');
+
+-- Grant all privileges on the database to root (if you want root to access from any host)
+GRANT ALL PRIVILEGES ON `DB_NAME`.* TO 'root'@'%' IDENTIFIED BY 'YOUR_SECURE_PASSWORD';
+
+-- Flush privileges to apply the changes
+FLUSH PRIVILEGES;
+
--- srcs/requirements/mariadb/tools/start-mariadb.sh
+++ srcs/requirements/mariadb/tools/start-mariadb.sh
@@ -4,46 +4,7 @@
 
 echo "Starting MariaDB server..."
 
-# Start the MariaDB server in the background
-mysqld_safe &
-mariadb_pid=$!
-
-echo "Waiting for MariaDB server to start..."
-while ! mysqladmin ping --silent; do
-	sleep 2
-done
-
-echo "MariaDB server started. Setting up user: ${MYSQL_USER}"
-run_cmd="mysql -u root -p${MYSQL_ROOT_PASSWORD} -e"
-
-# Create database
-echo "Create db if it doesn't exist"
-$run_cmd "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-
-# Create user
-echo "Create user if it doesn't exist"
-$run_cmd "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# Grant privileges
-echo "Grant all privileges on ${DB_NAME} to ${MYSQL_USER}"
-$run_cmd "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${MYSQL_USER}\`@'%';"
-
-# Flush privileges
-echo "Flush privileges"
-$run_cmd "FLUSH PRIVILEGES;"
-
-
-# CREATE DATABASE IF NOT EXISTS $DB_NAME;
-# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-# GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';
-# SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASSWORD');
-
-echo "Setup complete."
-
-# Wait for the background MariaDB server to stop
-wait $mariadb_pid
-
-echo "Launching Database as the main process."
+envsubst < secure_mariadb.sql > secure_mariadb_env.sql
 
 # Replace the current shell with mysqld_safe as the main process
-exec mysqld_safe
+exec mysqld_safe --init-file=/secure_mariadb_env.sql
